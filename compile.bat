bcd3e5b0e219b6efd7cce5f72808033a1d92df5b
Alexander Liptak <alexadner.liptak.jr@googlemail.com>
2017-09-02 22:48:17 +0100

    Edited to work with McStas miniconda3

--- compile.bat
+++ compile.bat
@@ -1,9 +1,13 @@
+@echo off
+setlocal
+PATH=C:\mcstas-2.4.1\miniconda3\Scripts;%PATH%
+@echo on
 pyinstaller ^
 	--distpath=./__TEMP__/dist ^
 	--workpath=./__TEMP__/build ^
+	--specpath=./__TEMP__/ ^
 	--noconfirm ^
 	--clean ^
 	--onefile ^
-	--specpath=./__TEMP__/ ^
 	--icon=./icon.ico ^
 	SWINE.py
